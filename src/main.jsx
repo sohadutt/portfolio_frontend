48f308017b0df3aacea75d9e3ee5faf8fbe3ab01
sohadutt <sohadutt@users.noreply.github.com>
2026-04-12 07:04:54 +0530

    added speeed insight and analytics also change from npm to pnpm

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react" // <-- Notice it says /react, not /next
 import './style/index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <SpeedInsights />
+    <Analytics />
   </StrictMode>,
 )
